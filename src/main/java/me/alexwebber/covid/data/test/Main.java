525e5f8b044170bae579917c0dff1d8f81bed89a
Alex Webber <webber512@live.missouristate.edu>
2020-05-19 20:30:12 -0500

    Removed old docs

--- src/main/java/me/alexwebber/covid/data/test/Main.java
+++ src/main/java/me/alexwebber/covid/data/test/Main.java
@@ -1,7 +1,14 @@
 package me.alexwebber.covid.data.test;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.util.ArrayList;
 import java.util.List;
+
+import com.opencsv.CSVReader;
+import com.opencsv.exceptions.CsvException;
 
 import me.alexwebber.covid.data.COVIDData;
 import me.alexwebber.covid.data.models.CountyInfo;
@@ -9,14 +16,22 @@ import me.alexwebber.covid.data.objects.StateListObject;
 
 public class Main {
 
-	public static void main(String[] args) throws IOException {
+	public static void main(String[] args) throws IOException, CsvException {
 		COVIDData covid = new COVIDData();
 
-		List<CountyInfo> countiesInKS = covid.county.getCountiesInState("WA");
-		System.out.print(countiesInKS.size());
-		for (CountyInfo c : countiesInKS) {
-			System.out.println(c.getCountyName());
+		URL dataStream = new URL("https://raw.githubusercontent.com/owid/covid-19-data/master/public/data/owid-covid-data.csv");
+		BufferedReader in = new BufferedReader(new InputStreamReader(dataStream.openStream()));
+		CSVReader csvReader = new CSVReader(in);
+		List<String[]> list = new ArrayList<>();
+		list = csvReader.readAll();
+		for (String[] s : list)
+		{
+			for (String ss : s) {
+				System.out.print(ss + "\t");
+			}
+			System.out.print("\n");
 		}
+		csvReader.close();
 
 	}
 
